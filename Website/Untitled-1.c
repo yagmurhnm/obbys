9e430b451e599178dbad838c2d8ea09cd23464de
yagmurhnm <ybt2455@gmail.com>
2022-09-07 22:42:07 +0300

    obbys only commit

---
+++ Website/Untitled-1.c
@@ -0,0 +1,3 @@
+o   RegisterAccount(): This method will register the user account to the smart contract.
+o   Start(): This command will mean to move for the robot car. This command will be executed only by the registered mobile user.
+o   Stop(): This command will mean to stop for the robot car. This command will be executed only by the registered mobile user.
